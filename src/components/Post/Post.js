5e27f03ebe7fdc7a0ba7a01220936526246c168d
Christian Paun <christianpaun@gmail.com>
2018-03-07 13:40:16 +0000

    getting data with axios

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -2,13 +2,19 @@ import React from 'react';
 
 import './Post.css';
 
-const post = (props) => (
-    <article className="Post">
-        <h1>Title</h1>
+const post = (props) => {
+    let title = props.title;
+    if (title.length>20 ) {title = props.title.substr(0,20)+'...'
+
+    }
+    return (
+    <article className="Post" onClick={props.clicked}>
+        <h1>{title}</h1>
         <div className="Info">
-            <div className="Author">Author</div>
+            <div className="Author">{props.author}</div>
         </div>
     </article>
-);
+    )
+};
 
 export default post;
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,18 +4,51 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from 'axios';
 
 class Blog extends Component {
+    
+    state = {
+        posts: [],
+        selectedPostId:null
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(resolve=>{
+            const postsArr = resolve.data.slice(0,4);
+            const updatedPosts = postsArr.map(e=>{
+                //console.log(e);
+                return {
+                    ...e,
+                    author:'Chris'
+                }
+            })
+            //console.log(updatedPosts);
+            this.setState({posts:updatedPosts});
+            //console.log(resolve, 'status:'+resolve.status);
+        });
+    }
+    postSelectedHandler = (id)=> {
+this.setState({selectedPostId:id})
+    };
+
     render () {
+        const posts = this.state.posts.map (e=>{
+            return <Post 
+            key={e.id} 
+            title={e.title} 
+            author={e.author}
+            clicked={()=>this.postSelectedHandler(e.id)}/>;
+        })
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
