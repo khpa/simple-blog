a1fff3625d7ce69190c471ff63cc97a19fa29048
Christian Paun <christianpaun@gmail.com>
2018-03-08 11:18:32 +0000

    axios interceptors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use((request)=>{
+console.log(request);
+//can edit before returning - the idea behind interceptors
+return request;
+}, (error)=>{
+    console.log(error);
+    return Promise.reject(error);
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
